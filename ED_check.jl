8e75daf63811054c3c119103b4f63564df68d29d
Bae Minwoo <101952394+bmw970712@users.noreply.github.com>
2023-03-12 14:20:31 +0900

    code revised2

--- ED_check.jl
+++ ED_check.jl
@@ -1,11 +1,11 @@
+using LinearAlgebra
+
 include("ED_XXZ.jl")
 
-L = 4
-Δ = 3
-
-k = 0
-N = 2
-
+L = 8
+Δ = 1
+N = 4
+#k = 0
 mat_arr = []
 
 basis_N = []
@@ -20,10 +20,11 @@ Build_Basis_N(config_arr, basis_N, N, L)
 #Idx_H_1D_XXZ(config_arr,Δ,L,mat_arr)
 Act_H_1D_XXZ(config_arr,Δ,L,H_mat)
 
-Build_Basis_N_k(basis_N, basis_N_k, L, k)
+#Build_Basis_N_k(basis_N, basis_N_k, L, k)
 
-print(length(basis_N_k))
-println("")
+#print(length(basis_N_k))
+#println("")
+
 #for each in mat_arr
 #    m = (Int)(each[1]) + 1
 #    n = (Int)(each[2]) + 1
@@ -42,13 +43,25 @@ H_N_mat = zeros(length(basis_N),length(basis_N))
 
 Build_H_N(H_mat, H_N_mat, basis_N)
 
+println(length(basis_N))
+#for m=1:2^L
+#    for n=1:2^L
+#        print(H_mat[m,n],"  ")
+#    end
+#    print("\n")
+#end
 
-for m=1:length(basis_N)
-    for n=1:length(basis_N)
-        print((Int)(H_N_mat[m,n]),"  ")
-    end
-    print("\n")
-end
+#for m=1:length(basis_N)
+#    for n=1:length(basis_N)
+#        print((H_N_mat[m,n]),"  ")
+#    end
+#    print("\n")
+#end
 
 #println("")
 #print(basis_N)
+
+print(eigvals(H_N_mat))
+println("")
+
+#print(eigvecs(H_N_mat))
